ecbce3c652be1a8af4dd68dfab738b3e80bad978
Evan Mahone <apathinwalking@gmail.com>
2015-06-16 11:46:08 -0400

    reorganized methods

--- dboa.js
+++ dboa.js
@@ -1,21 +1,15 @@
-exports.column = require('./lib/column/index.js');
-exports.table = require('./lib/table/index.js');
-exports.schema = require('./lib/schema/index.js');
-exports.db = require('./lib/db/index.js');
+var Column = require('./lib/column/index.js');
+var Table = require('./lib/table/index.js');
+var Schema = require('./lib/schema/index.js');
+var Db = require('./lib/db/index.js');
 
-
-/*var Q = require('q');
-var _columnQuery = require('./lib/column/query.js');
-var _tableQuery = require('./lib/table/query.js');
-var _tableCompare = require('./lib/table/compare.js');*/
-
-/*var Dboa = function(connection_options){
+var Dboa = function(connection_options){
     "use strict";
-    if(connection_options.client !== 'pg' || connection_options.client === undefined){
-        throw(new Error("Only pg is accepted as a client (for now)"));
-    }
-    this.knex = require('knex')(connection_options);
-};*/
+    var knex = require('knex')(connection_options);
+    this.column = new Column(knex);
+    this.table = new Table(knex);
+    this.schema = new Schema(knex);
+    this.db = new Db(knex);
+};
 
-
-//module.exports = Dboa;
+module.exports = Dboa;
