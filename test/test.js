dee5ef057e6a5f86dfce82e7c539accb5dd151df
Poco Umami <poco@localhost.localdomain>
2015-08-14 13:02:55 -0400

    oops

--- test/test.js
+++
@@ -1,5 +0,0 @@
-var dboa = require("../lib/index.js")({
-  client: "pg",
-  connection: "dbname=poco host=localhost port=5432 user=postgres password='password'"
-});
-var Promise = require('bluebird');
